9fff94e7d804a1968810f1bcc2c855b56d366b49
akazad1235 <akazad914@gmail.com>
2020-10-12 03:16:40 +0600

    appointment section added

--- src/components/MakeAppoinment/MakeAppoinment.js
+++ src/components/MakeAppoinment/MakeAppoinment.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import './MakeAppoinment.css'
+import doctors from '../../images/doctors.png'
 
 const MakeAppoinment = () => {
     return (
-        <div>
-            <h1>this is appoinment</h1>
-        </div>
+       <section className="make-appoinment">
+           <div className="container">
+                <div className="row">
+                    <div className="col-md-5">
+                    <img src={doctors} alt=""/>
+                    </div>
+                    <div className="col-md-7 text-white mt-5">
+                        <h3 className="text-primary">Appoinment</h3>
+                        <h2 className="text-white">Make An Appoinment <br/>Today</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora corporis alias accusantium. Ex accusantium voluptates, repellendus libero eligendi voluptatum inventore nobis omnis atque tempore dolore placeat necessitatibus? Assumenda, odio velit.</p>
+                        <button className="btn btn-primary">Lean More</button>
+                    </div>
+                </div>
+                
+           </div>
+       </section>
     );
 };
 
